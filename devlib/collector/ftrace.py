ARM-software/devlib

commit e0c53d09990b5501e493d048a5dce067d8990281

ftrace: Set top-level buffer size
trace-cmd start -B devlib -b 42 will set the buffer size for the
"devlib" ftrace instance but will not set the buffer size of the
top-level buffer.

Unfortunately, some events still end up in the top-level buffer
regardless of any configuration such as the "print" event. This can lead
to lost events because the buffer size was too small.

Avoid that by using the buffer size for both top-level and devlib's
instance.

devlib/collector/ftrace.py
@@ -240,6 +240,17 @@ class FtraceCollector(CollectorBase):
     def reset(self):
         self.target.execute('{} reset -B devlib'.format(self.target_binary),
                             as_root=True, timeout=TIMEOUT)
+
+        # trace-cmd start will not set the top-level buffer size if passed -B
+        # parameter, but unfortunately some events still end up there (e.g.
+        # print event). So we still need to set that size, otherwise the buffer
+        # might be too small and some event lost.
+        if self.buffer_size is not None:
+            self.target.write_value(
+                self.target.path.join(self.tracing_path, 'buffer_size_kb'),
+                self.buffer_size
+            )
+
         if self.functions:
             self.target.write_value(self.function_profile_file, 0, verify=False)
         self._reset_needed = False

commit 636ec62a8c271dd6cafe3d5de517cc00ff80a8af

ftrace: Separate top_buffer_size from buffer_size
Since we now set the top buffer size to be the same as the devlib buffer
size, this effectively halves the maximum available buffer size that can
be set while using devlib. Whatever size is passed as `buffer_size` will
be allocated twice, even if the top buffer is hardly used at all.

This commit separates them into `buffer_size` and `top_buffer_size`. If
the latter is not passed, the behaviour will not change compared to now.

Fixes: e0c53d09990b5501e493d048a5dce067d8990281
Signed-off-by: Kajetan Puchalski <kajetan.puchalski@arm.com>

devlib/collector/ftrace.py
@@ -60,6 +60,7 @@ class FtraceCollector(CollectorBase):
                  tracer=None,
                  trace_children_functions=False,
                  buffer_size=None,
+                 top_buffer_size=None,
                  buffer_size_step=1000,
                  tracing_path=None,
                  automark=True,
@@ -77,6 +78,7 @@ class FtraceCollector(CollectorBase):
         self.tracer = tracer
         self.trace_children_functions = trace_children_functions
         self.buffer_size = buffer_size
+        self.top_buffer_size = top_buffer_size
         self.tracing_path = self._resolve_tracing_path(target, tracing_path)
         self.automark = automark
         self.autoreport = autoreport
@@ -245,10 +247,11 @@ class FtraceCollector(CollectorBase):
         # parameter, but unfortunately some events still end up there (e.g.
         # print event). So we still need to set that size, otherwise the buffer
         # might be too small and some event lost.
-        if self.buffer_size is not None:
+        top_buffer_size = self.top_buffer_size if self.top_buffer_size else self.buffer_size
+        if top_buffer_size:
             self.target.write_value(
                 self.target.path.join(self.tracing_path, 'buffer_size_kb'),
-                self.buffer_size
+                top_buffer_size,
             )
 
         if self.functions: