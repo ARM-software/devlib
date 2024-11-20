e402fc754406b688983787b0527ed6d8594da14e
Douglas Raillard <douglas.raillard@arm.com>
2024-11-20 12:53:12 +0000

    target: Make Target.make_temp() use Target.tmp_directory
    Also avoid a "None" prefix when no prefix is asked for, and set None as
    the default prefix value.
    
    Remove the "devlib-test" default value as make_temp() has nothing to do
    with tests.

--- devlib/target.py
+++ devlib/target.py
@@ -1215,7 +1215,7 @@ fi
                 raise
 
     @asyn.asynccontextmanager
-    async def make_temp(self, is_directory=True, directory='', prefix='devlib-test'):
+    async def make_temp(self, is_directory=True, directory=None, prefix=None):
         """
         Creates temporary file/folder on target and deletes it once it's done.
 
@@ -1233,10 +1233,11 @@ fi
         :rtype: str
         """
 
-        directory = directory or self.working_directory
+        directory = directory or self.tmp_directory
+        prefix = f'{prefix}-' if prefix else ''
         temp_obj = None
         try:
-            cmd = f'mktemp -p {quote(directory)} {quote(prefix)}-XXXXXX'
+            cmd = f'mktemp -p {quote(directory)} {quote(prefix)}XXXXXX'
             if is_directory:
                 cmd += ' -d'
 
